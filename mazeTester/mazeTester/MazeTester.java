a2eeb8826e483d3a481a0566d7132b7e6a6b80eb
M. Corey Glass <dhexcxr@gmail.com>
2021-12-23 15:47:39 -0500

    another test 3.47p

--- mazeTester/mazeTester/MazeTester.java
+++ mazeTester/mazeTester/MazeTester.java
@@ -5,6 +5,7 @@ public class MazeTester {
 	public static void main(String[] args) {
 		// start new repo
 		// more changes to see what happens when I push
+		// how long do things take to appear on githu
 
 	}
 
